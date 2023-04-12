bfb5ed71d242b78f76433634a4c48bc2c52cb5c9
Dasha <93427597+DashaAga@users.noreply.github.com>
2023-04-12 22:21:29 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,15 +1,14 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import shapiro
+import scipy.stats as st
 
 chat_id = 588908837 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    alpha=0.03
-    statistic1, pvalue1 = shapiro(x)
-    statistic2, pvalue2 = shapiro(y)
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    alpha = 0.04
+    a = st.anderson_ksamp([x, y]).pvalue
     
-    if pvalue1 < alpha or pvalue2 < alpha:
-        return True
-    else:
-        return False
+    return a < alpha
