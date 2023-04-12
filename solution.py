cf28454d5e5b5c1f50c799a90fab848766301c47
Dasha <93427597+DashaAga@users.noreply.github.com>
2023-04-12 11:07:29 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import mannwhitneyu
+from scipy.stats import shapiro
 
 chat_id = 588908837 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     alpha=0.03
+    statistic1, pvalue1 = shapiro(x)
+    statistic2, pvalue2 = shapiro(y)
     stat, p = mannwhitneyu(x, y, alternative='two-sided')
-    return p < alpha
+    if pvalue1 < alpha or pvalue2 < alpha:
+        return True
+    else:
+        return False
