f6f3bbc1f2b9bea11f1f4c22c28704e0058643be
Dasha <93427597+DashaAga@users.noreply.github.com>
2023-04-12 10:36:37 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,10 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ks_2samp
+from scipy.stats import mannwhitneyu
 
 chat_id = 588908837 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     alpha=0.03
-    stat, p_val = ks_2samp(x, y)
-
-    if p_val < alpha:
-        return True
-    else:
-        return False
+    stat, p = mannwhitneyu(x, y, alternative='two-sided')
+    return p < alpha
